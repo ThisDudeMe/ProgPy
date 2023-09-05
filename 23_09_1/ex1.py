e76610f58aeeebaa3649ef34b5e590eb47624372
ThisDudeMe <ervin398@gmail.com>
2023-09-05 16:00:27 +0200

    gfgfg

---
+++ 23_09_1/ex1.py
@@ -0,0 +1,33 @@
+userName = str(input("Whats your name?"))
+
+print(" Hi! ",userName, "Im SUPPER AI 2000! Its nice to meet you!")
+
+print ("And how old are you ",userName, "?")
+
+while True:
+    
+    userAge = int(input("Please type your age!"))
+    if userAge == int:
+
+        if userAge <= 5:
+                print("dfdsfdfgs")
+
+        elif userAge > 5 and userAge <= 12:
+            print("sdsdfdfsg")
+    
+        elif userAge > 12 and userAge <= 17:
+            print("dfghd")
+    
+        elif userAge >= 18:
+            print("sddfdghhjg")
+
+        break
+    
+    else:
+        print("wrong input bla bla bal")
+        #dfgfff
+            
+        
+   
+    
+ 
