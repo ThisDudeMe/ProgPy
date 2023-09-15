7244af75e4cee16a560976812b5508a712f1a492
ThisDudeMe <ervin398@gmail.com>
2023-09-15 14:18:01 +0200

    commitworking on Def add card

--- SideProjects/Other/testBlackJack.py
+++ SideProjects/Other/testBlackJack.py
@@ -1,5 +1,9 @@
 import random
 
+from colorama import Fore, Back, Style, init
+from sympy import false
+
+
 def BlackJackCards():
 
     cardTypes = {"Ace": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Joker": 10, "Queen": 10, "King": 10}
@@ -17,15 +21,16 @@ def BlackJackShuffle():
     random.shuffle(shuffledCardStack)
 
     return shuffledCardStack
-def BlackJackRules():
+def BlackJackStartDeal():
 
-    currentgameCards = BlackJackShuffle()
+    currentGameCards = BlackJackShuffle()
     aiCards = []
     playerCards = []
     isAiTurn = True
     cantAddMoreAi = False
     cantAddMorePlayer = False
     dealFinnished = False
+    aiAllowShowFirsCard = True
 
     while dealFinnished == False:  
         
@@ -38,10 +43,18 @@ def BlackJackRules():
             if cantAddMoreAi == False:
 
                 if isAiTurn == True:
+                    
                             
                     if len(aiCards) < 2:
                                 
-                        aiCards.append(currentgameCards.pop())
+                        aiCards.append(currentGameCards.pop())
+
+                        if aiAllowShowFirsCard == False:
+                            print("AI: ","Card")
+                        
+                        if aiAllowShowFirsCard == True:
+                            print("AI: ",len(aiCards[-1]))
+                            aiAllowShowFirsCard = False
                     
 
                     else:
@@ -55,7 +68,9 @@ def BlackJackRules():
 
                     if len(playerCards) < 2:
 
-                        playerCards.append(currentgameCards.pop())
+                        playerCards.append(currentGameCards.pop())
+                        print("Player: ", playerCards[-1])
+
                 
                             
                     else:
@@ -63,12 +78,95 @@ def BlackJackRules():
 
                 isAiTurn = True
 
-    return aiCards,playerCards,currentgameCards
+    return aiCards,playerCards,currentGameCards
+def AddCard():
+    #SWITCH DYNAMIC TO PLAYER AND AI //// CURRENTLY DOES NOT ADD CARDS FROM DECK AND REMOVE FROM DECK!!
+    cardTypes = BlackJackCards()
+    cards = BlackJackStartDeal()
+    aiCards, playerCards,currentGameCards = cards
+    dynamicCards = []
+    dynamicCardsValue = 00
+    playerCardValue = 0
+    aiCardValue = 0
+    isAiTurn = False
 
-afterDeal = BlackJackRules()
+    if isAiTurn is False:
 
-pCards,aCards,cDeck = afterDeal
+        dynamicCards = playerCards
+        dynamicCardsValue = playerCardValue
+
+    if isAiTurn is True:
+        
+        dynamicCards = aiCards
+        dynamicCardsValue = aiCardValue
+
+    for i in range(dynamicCards[-1]):
+
+        if i in cardTypes:
+            
+            if "Ace" in dynamicCards:
+                
+                if cardTypes["Ace"] == 1:
+                    
+                    if (dynamicCardsValue + cardTypes[i] + 10) <= 21:
+
+                        cardTypes["Ace"] = 11
+                        dynamicCardsValue += cardTypes[i]
+
+                if cardTypes["Ace"] == 11:
+
+                    if (dynamicCardsValue + cardTypes[i]) > 21:
+
+                        cardTypes["Ace"] = 1
+                        dynamicCardsValue += cardTypes[i]
+
+                else:
+                    cardTypes["Ace"] = 1
+                    dynamicCardsValue += cardTypes[i]      
+
+            else:
+        
+                dynamicCardsValue += cardTypes[i]                
+    
+    if isAiTurn == False:
+
+        playerCardValue = dynamicCardsValue
+        
+        return isAiTurn, playerCardValue, aiCardValue
+    
+    if isAiTurn == True:
+        
+        aiCardValue = dynamicCardsValue
+        return isAiTurn, aiCardValue
+def AITrigger():
+    aIcards = AddCard()
+    isAITurn, aiCardValue = aIcards
+def StartGame():
+    while True:
+        
+        userInput = input((Fore.GREEN,"Press: C for CONTINUE! ",Fore.RED,"Press: S for STOP! ", Fore.WHITE ))
+
+        if userInput == "C":
+            playercards = AddCard()
+            isaiTurn, playerCardValue = playercards
+            AddCard()
+
+        if userInput == "S":
+            
+            AITrigger()
+            break
+            
+        else:
+            print("Unknown command! Please presss the correct key! ")
+
+    
+
+    
+        
 
 
-print(f"playercards: {pCards}", f"aiCards: {aCards}", f"deckremain: {cDeck}")
+    
+
+
+
 
