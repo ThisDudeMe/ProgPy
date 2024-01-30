3707b54a5ffc7599174e2caef8036b8ab36e7531
ThisDudeMe <ervin398@gmail.com>
2024-01-30 09:13:57 +0100

    vcvbhnm,

--- 24_01_10/app.py
+++ 24_01_10/app.py
@@ -4,17 +4,29 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/hello")
-def hellow_world():
-    return "Hello World"
+def hello_world():
+    return "Hello world"
 
 
-app.route("/submit", methods = ["POST"])
+@app.route("/submit", methods=["POST"])
 def submit():
     print(request.json)
     json_body = request.json
-    return("message")
+    print(json_body["title"])
+    # Databaslogik
+    return {"message": "Data submitted successfully!"}
 
-app.route("/books/<requested_book>")
+@app.route("/books/<requested_book>")
 def books(requested_book):
     print(requested_book)
-    return "SomethingSomethingBook"
+    return "Lorem ipsum"
+
+@app.route("/books")
+def get_books():
+    print(request.args)
+    return "Lorem ipsum"
+
+
+
+
+
