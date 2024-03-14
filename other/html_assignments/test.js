ce969a21ea899d950d3dff35f1ccb6691ab07e17
Ervin-me <ervin398@gmail.com>
2024-03-14 08:03:59 +0100

    sadsdfsdfds

---
+++ other/html_assignments/test.js
@@ -0,0 +1,22 @@
+document.addEventListener
+(
+    'DOMContentLoaded', function() 
+    {
+        var image = document.getElementById("arrowgreen");
+        setInterval
+        (
+            function()
+            {
+                if (image.style.visibility === "hidden")
+                {   
+                    image.style.visibility = "visible";
+                }
+                else
+                {
+                    image.style.visibility = "hidden";
+                }
+            }
+            , 1000
+        );
+    }
+);
